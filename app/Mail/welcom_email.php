a1b2eb251aec57350671fd67587ffe5a66ccd374
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-03-04 14:06:20 +0000

    almost did the email message

---
+++ app/Mail/welcom_email.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+
+class welcom_email extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public $message;
+    public $subject;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($message, $subject)
+    {
+        $this->message = $message;
+        $this->subject = $subject;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('jeffrey@example.com', 'Jeffrey Way'),
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'welcom_email.blade.php',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
